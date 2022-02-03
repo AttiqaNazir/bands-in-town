f4d092e394bb1cf539cf891f1f9430a19cae6fac
Attiqa Nazir <atiqanazir15@gmail.com>
2022-02-04 01:11:18 +0500

    extra files deleted and essential components generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { ArtistComponent } from './components/artist/artist.component';
+import { EventComponent } from './components/event/event.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchBarComponent,
+    ArtistComponent,
+    EventComponent
   ],
   imports: [
     BrowserModule,
